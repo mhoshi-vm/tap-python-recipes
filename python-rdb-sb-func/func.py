54b8ae1deeecc2bda7f7c14610cdfd35675bf6a1
machih <machih@vmware.com>
2022-12-23 11:25:17 +0900

    Fix binding

--- python-rdb-sb-func/func.py
+++ python-rdb-sb-func/func.py
@@ -4,30 +4,23 @@ from typing import Any
 
 
 def main(req: Any):
-    host = ""
-    port = ""
-    database = ""
-    username = ""
-    password = ""
-
     try:
         sb = binding.ServiceBinding()
         print(sb.all_bindings())
-        bindings_list = sb.bindings("aaaa")
-        if not bindings_list[0]:
+        bindings_list = sb.bindings('postgresql')
+        if bindings_list:
             host = bindings_list[0].get("host")
             port = bindings_list[0].get("port")
             database = bindings_list[0].get("database")
             username = bindings_list[0].get("username")
             password = bindings_list[0].get("password")
+            db_url = 'pq://' + username + ":" + password + "@" + host + ":" + port + "/" + database
+            """
+            db = postgresql.open()
+            get_table = db.prepare("SELECT * from information_schema.tables WHERE table_name = $1")
+            """
     except binding.ServiceBindingRootMissingError as msg:
         # log the error message and retry/exit
         print("SERVICE_BINDING_ROOT env var not set")
-
-    db_url = 'pq://' + username + ":" + password + "@" + host + ":" + port + "/" + database
-
-    """
-    db = postgresql.open()
-    get_table = db.prepare("SELECT * from information_schema.tables WHERE table_name = $1")
-    """
-    return db_url
+    else:
+        return db_url
