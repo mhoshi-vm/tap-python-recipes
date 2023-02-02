b8b6b109ec0dbf8cb99e165764aa0720775b991f
mhoshi-vm <67083789+mhoshi-vm@users.noreply.github.com>
2023-02-02 22:23:22 +0900

    Update func.py
    Return null event

--- python-simple-func-event/func.py
+++ python-simple-func-event/func.py
@@ -3,4 +3,4 @@ from typing import Any
 def main(data: Any, attributes: dict):
     print("Attributes" + str(attributes))
     print("Handled cloudevent!")
-    return attributes, "Handled cloudevent!"
+    return None, "Handled cloudevent!"
